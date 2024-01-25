c914276368aa994a1ec8347517d5780b96ee04ee
Rachel Nascimento <rachel@pop-os.localdomain>
2024-01-25 11:21:00 +0100

    deploy tp-support

---
+++ web/cerise.php
@@ -0,0 +1,8 @@
+<?php
+
+//DO NOT MODIFY THIS FILE
+
+if(!empty(file_get_contents("./bigF")))
+{ 
+    echo 'File loaded!'; 
+}
---
+++ web/poire.php
@@ -0,0 +1,11 @@
+<?php
+
+//DO NOT MODIFY THIS FILE
+
+function hello(string $node_version) : void
+{
+    if($node_version == "14") echo "This is it!";
+    else throw new Exception("Wrong version: require node version 14");
+}
+
+hello(getenv("CC_NODE_VERSION"));
